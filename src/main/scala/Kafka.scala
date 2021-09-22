8182f9965cd379bbea48f0b56d4a964a720d4e95
Andrey.Vorontsov <andrei.vorontsov@nordigy.ru>
2021-09-23 01:40:02 +0300

    Refactor

--- src/main/scala/Kafka.scala
+++ src/main/scala/Kafka.scala
@@ -1,35 +1,70 @@
-import Kafka.{getBooks, stateUpdate}
 import Library.BookEvent
 import cats.effect._
 import cats.effect.unsafe.implicits.global
 import fs2.kafka._
 
-object Kafka{
+class Kafka[F[_]](state: Ref[IO, Map[String, Book]]) {
   val deserializer: Deserializer[IO, Either[String, BookEvent]] =
-    Deserializer.instance[IO, Either[String, BookEvent]] { case (_, _, payload) =>
-      IO.pure(Option(BookEvent.parseFrom(payload)).toRight("Error"))
+    Deserializer.instance[IO, Either[String, BookEvent]] {
+      case (_, _, payload) =>
+        IO.pure(Option(BookEvent.parseFrom(payload)).toRight("Error"))
     }
   val consumerSettings: ConsumerSettings[IO, Unit, Either[String, BookEvent]] =
-    ConsumerSettings[IO, Unit, Either[String, BookEvent]](Deserializer.unit[IO],deserializer)
-      .withAutoOffsetReset(AutoOffsetReset.Earliest)
-      .withBootstrapServers("localhost:9092").withGroupId("group")
-  val stateIo: IO[Ref[IO, Map[String, Book]]] = Ref[IO].of(Map.empty[String,Book])
-  val stream: fs2.Stream[IO, BookEvent] =
-    KafkaConsumer.stream(consumerSettings)
-      .subscribeTo("book-events")
-      .records.map(_.record.value.getOrElse(BookEvent()))
+    ConsumerSettings[IO, Unit, Either[String, BookEvent]](
+      Deserializer.unit[IO],
+      deserializer
+    ).withAutoOffsetReset(AutoOffsetReset.Earliest)
+      .withBootstrapServers("localhost:9092")
+      .withGroupId("test-consumer-group")
 
-  val stateUpdate: fs2.Stream[IO, Unit] = stream.evalMap(event => stateIo.flatMap{ state => state.get.flatMap{ map => map.get(event.id) match {
-    case None => state.update(_.+(event.id-> Book(event.id,event.name,event.year,event.year)))
-    case Some(value) => state.update{m =>
-      val history = m(value.id).history
-      m.+(event.id -> Book(event.id, event.name,event.year,history ++ ", "++ event.year))
+  val kafkaConsumer
+      : fs2.Stream[IO, KafkaConsumer[IO, Unit, Either[String, BookEvent]]] =
+    KafkaConsumer
+      .stream(consumerSettings)
+      .subscribeTo("book-events")
+
+  val stateUpdate: BookEvent => IO[Unit] = event =>
+    state.get.flatMap { map =>
+      map.get(event.id) match {
+        case None =>
+          state.update(
+            _.+(
+              event.id -> Book(
+                event.id,
+                event.name,
+                event.year,
+                event.year
+              )
+            )
+          )
+        case Some(value) =>
+          state.update { m =>
+            val history = m(value.id).history
+            m.+(
+              event.id -> Book(
+                event.id,
+                event.name,
+                event.year,
+                history ++ ", " ++ event.year
+              )
+            )
+          }
+      }
     }
-  }}})
-  val getBooks = stateIo.unsafeRunSync().get.unsafeRunSync()
+  val stream =
+    kafkaConsumer.records
+      .map(_.record.value.getOrElse(BookEvent()))
+      .evalMap(stateUpdate)
 }
-object Main extends App {
-  stateUpdate.compile.drain.unsafeRunSync()
-  println(getBooks)
+
+object Main extends IOApp {
+  override def run(args: List[String]): IO[ExitCode] = {
+    val stateIo: IO[Ref[IO, Map[String, Book]]] =
+      Ref[IO].of(Map.empty[String, Book])
+    for {
+      ref <- stateIo
+      _ <- new Kafka[IO](ref).stream.compile.drain
+    } yield (ExitCode.Success)
+  }
 }
 case class Book(id: String, name: String, year: String, history: String)
